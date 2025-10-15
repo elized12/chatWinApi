c71f1407040e34f17f7bad4c14f2bbf9cbb8072d
Elized <frolov_k2004@mail.ru>
2025-10-15 13:53:17 +0300

    [FIX] Удалена папка build

--- build/CMakeFiles/ChatWinApi.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ChatWinApi.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/elized12/Desktop/projects/c++/chatWinApi/build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
